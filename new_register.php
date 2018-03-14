8d49ae591589dbf1bc232c1e0b49749af96213a6
Letícia <37351336+littledarven@users.noreply.github.com>
2018-03-14 04:12:04 -0300

    first commit

--- new_register.php
+++ new_register.php
@@ -12,46 +12,46 @@
     <header>
         <h1>Cadastro</h1>
     </header>
-<div class="row">
-        <section class="col-md-9">
-            <form method="POST" action="new.php" >
-                <div class="form-group row">
-                    <label for="fantasy_name" class="col-sm-2 col-form-label">Nome Fantasia</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="fantasy_name" name="fantasy_name"> 
+    <div class="row">
+            <section class="col-md-9">
+                <form method="POST" action="new.php" >
+                    <div class="form-group row">
+                        <label for="fantasy_name" class="col-sm-2 col-form-label">Nome Fantasia</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="fantasy_name" name="fantasy_name"> 
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="social_reason" class="col-sm-2 col-form-label">Razão Social</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="social_reason" name="social_reason">
+                    <div class="form-group row">
+                        <label for="social_reason" class="col-sm-2 col-form-label">Razão Social</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="social_reason" name="social_reason">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="email" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="email" name="email">
+                    <div class="form-group row">
+                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="email" name="email">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="phone" class="col-sm-2 col-form-label">Telefone</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="phone" name="phone">                  
+                    <div class="form-group row">
+                        <label for="phone" class="col-sm-2 col-form-label">Telefone</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="phone" name="phone">                  
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="cnpj" class="col-sm-2 col-form-label">CNPJ</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="cnpj" name="cnpj">                  
+                    <div class="form-group row">
+                        <label for="cnpj" class="col-sm-2 col-form-label">CNPJ</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="cnpj" name="cnpj">                  
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-10">
-                        <button type="submit" class="btn btn-primary">Cadastrar</button>
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Cadastrar</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </section>
-    </div>
+                </form>
+            </section>
+       </div>
 </body>
 </html>
